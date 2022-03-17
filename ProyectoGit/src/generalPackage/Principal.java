f809d3a2760c90e20c27e4edbc5db12649aebf09
KartashAnt <101747771+KartashAnt@users.noreply.github.com>
2022-03-17 20:36:09 +0100

    Ver 6
    quito un syso

--- ProyectoGit/src/generalPackage/Principal.java
+++ ProyectoGit/src/generalPackage/Principal.java
@@ -2,7 +2,6 @@ package generalPackage;
 
 public class Principal {
 	public static void main(String[] args) {
-		System.out.println("syso");
 		System.out.println("otro");
 		System.out.println("master");
 		System.out.println("Subida conectada con GitHub");
